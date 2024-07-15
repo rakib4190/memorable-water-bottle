a5b46c101c3c8631a3f973db70252536bd5b62cf
Rakibul Islam <mrislam4141@gmail.com>
2024-07-15 18:43:50 +0600

    handler added to bottles component

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Bottle.css'
-const Bottle = ({bottle}) => {
+const Bottle = ({bottle, handleAddToCart}) => {
     //console.log(bottle)
     const { name, img,price } = bottle;
     return (
@@ -8,6 +8,7 @@ const Bottle = ({bottle}) => {
             <h3> {name} </h3>
             <img src={img} alt="" />
             <p>Price: ${price} </p>
+            <button onClick={()=>handleAddToCart(bottle)} >Add to Cart</button>
         </div>
     );
 };
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -4,6 +4,14 @@ import './Bottles.css'
 
 const Bottles = () => {
     const [bottles, setBottles] = useState([]);
+    const [cart, setCart] = useState([]);
+    const handleAddToCart = bottle =>{
+        //console.log('add this to the cart item');
+        const cartBottle = [...cart, bottle];
+        //console.log(cartBottle);
+        setCart(cartBottle);
+    }
+
     useEffect(() =>{
         fetch('bottle.json')
         .then(res => res.json())
@@ -12,11 +20,16 @@ const Bottles = () => {
     return (
         <div>
             <h1>Memorable Bottle are: {bottles.length}</h1>
+            <div>
+                <h2>Cart Items: {cart.length} </h2>
+
+            </div>
             <div className='bottle-container'>
             {
                 bottles.map(bottle => <Bottle 
                     bottle = {bottle}
                     key={bottle.id}
+                    handleAddToCart= {handleAddToCart}
                     ></Bottle>)
             }
 
